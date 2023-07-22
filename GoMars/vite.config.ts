b2af6355bfba056853062eec84b082d591d9deb2
최은석 <trgf456@naver.com>
2023-07-22 12:22:37 +0900

    [add] folder coding start kit

---
+++ GoMars/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": "/src",
+      "@page": "/src/page",
+      "@components": "/src/components",
+      "@util": "/src/util",
+      "@type": "/src/type",
+    },
+  },
+});
