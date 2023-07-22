8e019179bafe6a9a9bec6a8ec599bce7d8e80a52
최은석 <trgf456@naver.com>
2023-07-23 03:55:59 +0900

    오늘까지한거 커밋

--- GoMars/src/layout/header/HomeHeader.tsx
+++ GoMars/src/layout/header/HomeHeader.tsx
@@ -1,169 +1,159 @@
 import styled from "styled-components";
 
-const HomeHeaderContainer = styled.div`
-  width: 100%;
+const HomeHeaderDiv = styled.div`
+  position: relative;
   align-items: stretch;
   display: flex;
   flex-direction: column;
-  z-index: 3;
+`;
+
+const HomeHeaderContainer = styled(HomeHeaderDiv)`
   backface-visibility: hidden;
+  align-items: stretch;
+  background-color: rgba(255, 255, 255, 0.85);
+  backdrop-filter: blur(12px);
+  display: flex;
+  flex-direction: column;
+  z-index: 3;
   position: sticky;
   top: -0.5px;
-  .homeheader-wrraper {
-    width: 100%;
+`;
+
+const HomeHeaderTop = styled(HomeHeaderDiv)`
+  z-index: 2;
+  cursor: pointer;
+  height: 53px;
+  padding: 0 16px;
+  align-items: center;
+  flex-direction: row;
+  justify-content: center;
+  width: 100%;
+`;
+const HomeHeaderTopTitle = styled(HomeHeaderDiv)`
+  height: 100%;
+  justify-content: center;
+  flex-grow: 1;
+  flex-shrink: 1;
+  align-items: flex-start;
+  h2 {
+    direction: ltr;
+    unicode-bidi: isolate;
+    background-color: rgba(0, 0, 0, 0);
+    list-style: none;
+    text-align: inherit;
+    display: inline;
+    max-width: 100%;
+    overflow-x: hidden;
+    overflow-y: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    padding-bottom: 2px;
+    padding-top: 2px;
+    word-wrap: break-word;
+    font-weight: 700;
+    color: rgb(15, 20, 25);
+    font-size: 20px;
+    line-height: 24px;
+    span {
+      white-space: inherit;
+      word-wrap: break-word;
+    }
+  }
+`;
+
+const HomeHeaderBottom = styled(HomeHeaderDiv)`
+  border-bottom: 1px solid rgb(239, 243, 244);
+  flex-direction: row;
+  align-items: center;
+  flex-basis: 0%;
+  height: 100%;
+  flex-grow: 1;
+  flex-shrink: 1;
+  outline-style: none;
+  display: block;
+  overflow: hidden;
+  .scroll-padding {
+    scroll-padding: 0 36px;
+    overflow-x: auto;
+    transform: translate3d(0px, 0px, 0px);
+    scroll-snap-type: x mandatory;
+    overflow-y: hidden;
+    flex-wrap: nowrap;
+    height: 100%;
+    flex-direction: row;
+    position: relative;
     align-items: stretch;
     display: flex;
     flex-direction: column;
-    position: relative;
-    backdrop-filter: blur(12px);
-    background-color: rgba(255, 255, 255, 0.85);
-    .homeheader-top-container {
-      width: 100%;
-      align-items: stretch;
+    .flex-row {
       display: flex;
-      flex-direction: column;
-      position: relative;
-      cursor: pointer;
-      z-index: 2;
-      .homeheader-top-wrraper {
-        cursor: pointer;
-        width: 100%;
-        align-items: stretch;
-        display: flex;
-        flex-direction: column;
-        position: relative;
-        z-index: 2;
-        height: 53px;
-        .homeheader-top-box {
-          cursor: pointer;
-          position: relative;
-          display: flex;
-          padding: 0 16px;
-          align-items: center;
-          flex-direction: row;
-          width: 100%;
-          height: 53px;
-          .homeheader-top-title {
-            cursor: pointer;
-            display: flex;
-            position: relative;
-            align-items: flex-start;
-            h2 {
-              direction: ltr;
-              unicode-bidi: isolate;
-              background-color: rgba(0, 0, 0, 0);
-              list-style: none;
-              text-align: inherit;
-              text-decoration: none;
-              display: inline;
-              font:
-                14px -apple-system,
-                BlinkMacSystemFont,
-                "Segoe UI",
-                Roboto,
-                Helvetica,
-                Arial,
-                sans-serif;
-
-              max-width: 100%;
-              overflow-x: hidden;
-              overflow-y: hidden;
-              text-overflow: ellipsis;
-              white-space: nowrap;
-              padding-bottom: 2px;
-              padding-top: 2px;
-              min-width: 0px;
-              word-wrap: break-word;
-              font-family:
-                "TwitterChirp",
-                -apple-system,
-                BlinkMacSystemFont,
-                "Segoe UI",
-                Roboto,
-                Helvetica,
-                Arial,
-                sans-serif;
-              font-weight: 700;
-              color: rgb(15, 20, 25);
-              font-size: 20px;
-              line-height: 24px;
-              .homeheader-title {
-                cursor: pointer;
-                list-style: none;
-                text-align: inherit;
-                display: inline;
-                color: inherit;
-                font: inherit;
-                white-space: inherit;
-                word-wrap: break-word;
-                font-family: inherit;
-              }
-            }
-          }
-        }
-      }
+      justify-content: center;
+      align-items: center;
     }
-    .homeheader-bottom-container {
-      align-items: stretch;
-      display: flex;
-      flex-direction: column;
-      position: relative;
-      nav {
-        display: flex;
-        position: relative;
-        z-index: 0;
-        align-items: center;
-        flex-direction: row;
-        border-bottom: 1px solid rgb(239, 243, 244);
-        .homeheader-bottom-wrraper {
-          display: flex;
-          flex-shrink: 0;
-          position: relative;
-          align-items: center;
-          outline-style: none;
-          flex-direction: row;
-          flex-grow: 1;
-          height: 100%;
-          flex-basis: 0%;
-          .homeheader-bottom-box {
-            align-items: stretch;
-            box-sizing: border-box;
-            flex-direction: column;
-            position: relative;
-            z-index: 0;
-            overflow: hidden;
-            display: block;
-            flex-grow: 1;
-            height: 100%;
-            flex-shrink: 1;
-            .homeheader-scrollpadding {
-              align-items: stretch;
-              display: flex;
-              flex-basis: auto;
-              position: relative;
-              z-index: 0;
-              flex-direction: row;
-              flex-grow: 1;
-              height: 100%;
-              flex-shrink: 1;
-              flex-wrap: nowrap;
-              overflow-y: hidden;
-              scroll-snap-type: x mandatory;
-              transform: translate3d(0px, 0px, 0px);
-              overflow-x: auto;
-              scroll-padding: 0px 36px;
-              .homeheader-following-container,
-              .homeheader-foryou-container {
-                align-items: stretch;
-                display: flex;
-                position: relative;
-                flex-grow: 1;
-                scroll-snap-align: start;
-              }
-            }
-          }
-        }
-      }
+  }
+`;
+
+const HomeHeaderBottomLeft = styled(HomeHeaderDiv)`
+  flex-grow: 1;
+  align-items: center;
+  transition: all 0.2s;
+  outline-style: none;
+  justify-content: center;
+  cursor: pointer;
+  flex-direction: column;
+  height: 53px;
+  min-width: 56px;
+  padding: 0 16px;
+  &:hover {
+    background-color: rgba(15, 20, 25, 0.1);
+  }
+
+  .rightbutton-title {
+    height: 100%;
+    padding-bottom: 16px;
+    padding-top: 16px;
+    font-weight: 500;
+    color: rgb(83, 100, 113);
+  }
+  .leftbutton-title {
+    font-weight: 700;
+    color: rgb(15, 20, 25);
+  }
+  .rightbutton-title,
+  .leftbutton-title {
+    direction: ltr;
+    unicode-bidi: isolate;
+    white-space: pre-wrap;
+    display: flex;
+    position: relative;
+    align-items: center;
+    word-wrap: break-word;
+    line-height: 20px;
+    flex-direction: row;
+    justify-content: center;
+    height: 100%;
+    span {
+      text-align: inherit;
+      color: inherit;
+      font: inherit;
+      white-space: inherit;
+      min-width: 0px;
+      word-wrap: break-word;
+      font-family: inherit;
+    }
+    .bottom-color {
+      border-bottom-left-radius: 9999px;
+      border-bottom-right-radius: 9999px;
+      border-top-left-radius: 9999px;
+      border-top-right-radius: 9999px;
+      display: inline-flex;
+      bottom: 0px;
+      position: absolute;
+      width: 100%;
+      min-width: 56px;
+      height: 4px;
+      align-self: center;
+      background-color: rgb(29, 155, 240);
     }
   }
 `;
@@ -171,31 +161,33 @@ const HomeHeaderContainer = styled.div`
 const HomeHeader = () => {
   return (
     <HomeHeaderContainer>
-      <div className="homeheader-wrraper">
-        <div className="homeheader-top-container">
-          <div className="homeheader-top-wrraper">
-            <div className="homeheader-top-box">
-              <div className="homeheader-top-title">
-                <h2>
-                  <span className="homeheader-title">Home</span>
-                </h2>
+      <HomeHeaderTop>
+        <HomeHeaderTopTitle>
+          <h2>
+            <span>Home</span>
+          </h2>
+        </HomeHeaderTopTitle>
+      </HomeHeaderTop>
+      <HomeHeaderBottom>
+        <div className="scroll-padding">
+          {/*  */}
+          <div className="flex-row">
+            <HomeHeaderBottomLeft>
+              <div className="leftbutton-title">
+                <span>For you</span>
+                <div className="bottom-color" />
               </div>
-            </div>
+            </HomeHeaderBottomLeft>
+            <HomeHeaderBottomLeft>
+              <div className="rightbutton-title">
+                <span>Following</span>
+                {/* <div /> */}
+              </div>
+            </HomeHeaderBottomLeft>
           </div>
+          {/*  */}
         </div>
-        <div className="homeheader-bottom-container">
-          <nav>
-            <div className="homeheader-bottom-wrraper">
-              <div className="homeheader-bottom-box">
-                <div className="homeheader-scrollpadding">
-                  <div className="homeheader-foryou-container"></div>
-                  <div className="homeheader-following-container"></div>
-                </div>
-              </div>
-            </div>
-          </nav>
-        </div>
-      </div>
+      </HomeHeaderBottom>
     </HomeHeaderContainer>
   );
 };
